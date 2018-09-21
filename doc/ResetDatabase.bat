44b0d151291da85edc819d55d3ed523759842a83
huulocpham1997 <huuloc1997>
2018-09-21 23:11:16 +0700

    fix name

--- doc/ResetDatabase.bat
+++ doc/ResetDatabase.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 ECHO Step 1: Input the password of root to drop the database if it exited.
-mysql -u root -p  < HKS_Drop_Database.sql
+mysql -u root -p  < Drop_Database.sql
 
 ECHO Step 2: Input the password of root to create the database.
-mysql -u root -p  < HKS_Script_CreateDB_MySQL.sql
+mysql -u root -p  < Script_CreateDB_MySQL.sql
 
 ECHO Step 3: Create tables for the database cfgl
-mysql -u cfgl -p123 cfgl < HKS_Script_Table_MYSQL.sql
+mysql -u cfgl -p123 cfgl < Script_Table_MYSQL.sql
 
 ECHO Step 4: Insert data into table for the database cfgl
-mysql -u cfgl -p123 cfgl < HKS_Example_Script_Table_MYSQL.sql
+mysql -u cfgl -p123 cfgl < Example_Script_Table_MYSQL.sql
 
 PAUSE
